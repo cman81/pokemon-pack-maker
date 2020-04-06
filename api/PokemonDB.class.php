956104a23900ce7af0855a29279bd79cdbebb0a1
Christian Manalansan <cmanalansan@gmail.com>
2020-04-06 08:01:00 -0400

    Fix file path to DB

--- api/PokemonDB.class.php
+++ api/PokemonDB.class.php
@@ -1,6 +1,6 @@
 <?php
     class PokemonDB extends SQLite3 {
         function __construct() {
-            $this->open('db/pokemon.db');
+            $this->open('../db/pokemon.db');
         }
     }
