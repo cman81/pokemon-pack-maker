69a2385b3fdd011bf8afa3e749d944e7bfe61dfe
Christian Manalansan <cmanalansan@gmail.com>
2020-04-10 07:35:48 -0400

    Prep to remove database from repo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# pokemon-pack-maker
+# Pokemon Pack Maker
+## Setup
+
+ 1. Create a sqlite database at db/pokemon.db
+ 2. Run db/create_db.sql
+ 3. Tweak the dataset in api/card_importer.php and run it
+ 4. Create a record in the profiles table: only the 'profile' field is required
+ 5. Tweak index.html for your profile
+ -- Target $('.load-profile')
---
+++ db/create_db.sql
@@ -0,0 +1,32 @@
+BEGIN TRANSACTION;
+DROP TABLE IF EXISTS "collections";
+CREATE TABLE IF NOT EXISTS "collections" (
+	"collection_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"profile_id"	TEXT,
+	"collection_name"	TEXT UNIQUE,
+	"collection_type"	TEXT,
+	"box_art"	TEXT
+);
+DROP TABLE IF EXISTS "card_collection_map";
+CREATE TABLE IF NOT EXISTS "card_collection_map" (
+	"ownership_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"collection_id"	TEXT,
+	"card"	TEXT
+);
+DROP TABLE IF EXISTS "cards";
+CREATE TABLE IF NOT EXISTS "cards" (
+	"card_id"	TEXT,
+	"rarity"	TEXT,
+	"expansion_set"	TEXT,
+	"market_value"	REAL,
+	PRIMARY KEY("card_id")
+);
+DROP TABLE IF EXISTS "profiles";
+CREATE TABLE IF NOT EXISTS "profiles" (
+	"profile_id"	TEXT,
+	"cash_added"	NUMERIC,
+	"wallet"	NUMERIC,
+	"packs_opened"	INTEGER,
+	PRIMARY KEY("profile_id")
+);
+COMMIT;
